2992a7eada50056566251a5060c15a21e74f9363
Ashadhananjaya <ashahdhananjaya@gmail.com>
2026-02-12 10:49:20 +0530

    qa
    i

--- demo.java
+++ demo.java
@@ -8,7 +8,17 @@ class Human{
 //     public String getName(){
 //         return name;
 //     }
+  public Human(){//default
+    //constructor will be called everytime a object is created now lets create 2nd objet obj1 in contructor will print 2 time
+    // System.out.println("in constructor");
+    age=12;
+    name="john";
 
+  }
+  public Human(int a ,String n){//parameterized
+    age=a;
+    name=n;
+  }
  private int age;
  private String name;
   public void setAge(int a){
@@ -30,9 +40,11 @@ public class demo {
     public static void main(String args[]) {
         //for encapsulation hiding we are  protecting by using private.. so we can use those variables only by using  method which will be in same cls to access or defien value we will use object and pass value through method so we are combining both variable and method so its encapsulation
         Human obj=new Human();
-       obj.setAge(30);
+        Human obj1=new Human(18,"johncc");
+    //    obj.setAge(30);
       
-       obj.setNmae("Ashaa");
+    //    obj.setNmae("Ashaa");
         System.out.println(obj.getAge()+": "+obj.getName());
+        System.out.println(obj1.getAge()+": "+obj1.getName());
     }
 }
