ea847740992d5c72ac15b7e9cb765754f5b6c1ec
Ashadhananjaya <ashahdhananjaya@gmail.com>
2026-02-19 16:35:07 +0530

    Updated demo files and inheritance example

--- Demo2.java
+++ Demo2.java
@@ -6,6 +6,7 @@ public A(){
    
         System.out.println("in B int");
     }
+  
 }
 class B extends A{
     public B(){
@@ -26,6 +27,7 @@ class B extends A{
 public class Demo2 {
     public static void main(String args[]){
         B obj=new B(5);
+        
     }
 
     
